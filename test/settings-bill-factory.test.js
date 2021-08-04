65e02fa2ffa0d455a74537475ba275600f247251
Jaden Hope <Jaden_Hope@Yahoo.com>
2021-08-04 12:39:16 +0200

    tests set to display output values

--- test/settings-bill-factory.test.js
+++ test/settings-bill-factory.test.js
@@ -11,6 +11,7 @@ describe('Settings Bill Factory:', () => {
         SettingsBill.recordAction('call');
 
         assert.deepEqual(SettingsBill.totals(), { callTotal: 20, grandTotal: 25, smsTotal: 5 });
+        console.log(SettingsBill.totals();
     })
     it('hasReachedWarningLevel function, warning level is not reached', () => {
         let SettingsBill = settingsBill();
@@ -21,6 +22,7 @@ describe('Settings Bill Factory:', () => {
         SettingsBill.recordAction('call');
 
         assert.equal(SettingsBill.hasReachedWarningLevel(), false);
+        console.log(SettingsBill.hasReachedWarningLevel());
     })
     it('hasReachedWarningLevel function, warning level is reached', () => {
         let SettingsBill = settingsBill();
@@ -30,6 +32,7 @@ describe('Settings Bill Factory:', () => {
         SettingsBill.recordAction('call');
 
         assert.equal(SettingsBill.hasReachedWarningLevel(), true);
+        console.log(SettingsBill.hasReachedWarningLevel());
     })
     it('hasReachedCriticalLevel function, ccritical level is not reached', () => {
         let SettingsBill = settingsBill();
@@ -39,6 +42,7 @@ describe('Settings Bill Factory:', () => {
         SettingsBill.recordAction('call');
 
         assert.equal(SettingsBill.hasReachedCriticalLevel(), false);
+        console.log(SettingsBill.hasReachedCriticalLevel());
     })
     it('hasReachedCriticalLevel function, critical level is reached', () => {
         let SettingsBill = settingsBill();
@@ -50,6 +54,7 @@ describe('Settings Bill Factory:', () => {
         SettingsBill.recordAction('call');
 
         assert.equal(SettingsBill.hasReachedCriticalLevel(), true);
+        console.log(SettingsBill.hasReachedCriticalLevel());
     })
     it('actions function, correct cost, type, and timestamp', () => {
         let SettingsBill = settingsBill();
@@ -58,5 +63,6 @@ describe('Settings Bill Factory:', () => {
         SettingsBill.recordAction('sms');
 
         assert.deepEqual(SettingsBill.actions(), [{  cost: 5,  timestamp: new Date(),  type: 'sms'}]);
+        console.log(SettingsBill.actions());
     })
 })
